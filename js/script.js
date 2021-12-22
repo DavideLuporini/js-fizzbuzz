b8fe6046142d99a580ee2b4b4a5377b2e4d3f57e
illupovero <davideluporini1996@gmail.com>
2021-12-22 16:41:38 +0100

    ADDED LIST in html + comments in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('js ok')
+console.log('JS OK!');
+
+// 1 creo una variabile di appoggio
+// 2 ciclo for da 1 a 100
+// 3) ciclo if per ogni caso 
+// 4) stampa il risultato in pagina
+
+// 1) Creo una variabile di appoggio
