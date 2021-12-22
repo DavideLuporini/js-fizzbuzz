1271bec0bd5044995776865e49eecb8f709509dd
illupovero <davideluporini1996@gmail.com>
2021-12-22 16:42:57 +0100

    added variable

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 console.log('JS OK!');
 
-// 1 creo una variabile di appoggio
+// 1 creo una variabile
 // 2 ciclo for da 1 a 100
 // 3) ciclo if per ogni caso 
 // 4) stampa il risultato in pagina
 
-// 1) Creo una variabile di appoggio
+
+
+// 1) Creo una variabile 
+let result = '';
+const numbers = document.getElementById('list-item');
