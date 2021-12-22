8bde726e43d2e7be8e2fe55b4e5abd6ed887753d
illupovero <davideluporini1996@gmail.com>
2021-12-22 16:50:26 +0100

    added  if cicles for all the cases

--- js/script.js
+++ js/script.js
@@ -9,10 +9,32 @@ console.log('JS OK!');
 
 // 1) Creo una variabile 
 let result = '';
-const numbers = document.getElementById('list-item');
+const numbers = document.getElementById('list');
 
 // 2) ciclo for da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
 
+    // 3) ciclo if per ogni caso 
+
+    // multipli  di 3
+    if (i % 3 === 0) {
+        result += '<li> Fizz </li>';
+        console.log(result)
+    }
+    // Multipli  di 5
+    if (i % 5 === 0) {
+        result += '<li> Buzz </li>';
+        console.log(result)
+    }
+    //Multipli di 3 e  5
+    if (i % 15 === 0) {
+        result += '<li> FizzBuzz </li>';
+        console.log(result)
+    }
+    // non multipli di 3 e 5
+    else {
+        result += `<li> ${i} </li>`;
+        console.log(result)
+    }
 }
